d3cb3ac33c748a424ce6116523c56302dc3d8132
Kristofer Soto <kristofersoto@outlook.com>
2026-04-17 22:33:09 -0400

    adding restoreState function to manage state while navigating between tabs

--- scripts/window.js
+++ scripts/window.js
@@ -3,13 +3,30 @@
  * @descr Restores application state based on the state object 
  ********************************************************/
 function restoreState(state) {
-    GlobalHistoryLogging = false;
-    if (GlobalDialogClose) {
-        GlobalDialogClose.click();
-        GlobalDialogClose = null;
-    }
-    // Note: Mode switching logic would go here in Phase 3
-    GlobalHistoryLogging = true;
+  console.log('Console: In restoreState: state:', JSON.stringify(state), 
+              " & GlobalDialogClose = ", GlobalDialogClose);
+  GlobalHistoryLogging = false;
+  if (GlobalDialogClose) {
+    GlobalDialogClose.click();
+    GlobalDialogClose = null;
+  }
+  switch (state.page) {
+    case "MODE_DIALOG":
+      GlobalModeActionButtons[state.mode].click();
+      GlobalDialogClose = GlobalDialogCancelButtons[state.mode];
+    break;
+    case "ACTIVITY_FEED":
+    case "ROUNDS":
+    case "COURSES":
+    case "BUDDIES":;
+      GlobalModeTabButtons[state.mode].click();
+      GlobalModeTabButtons[state.mode].focus();
+      break;
+    default:
+      console.log('restoreState not implemented for this page value:', state.page);
+      break;  
+  }
+  GlobalHistoryLogging = true;
 }
 
 window.addEventListener('load', (event) => {
