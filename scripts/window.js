61c4dc5e8b143fa48f0362bc46f720ddfcb71025
Kristofer Soto <kristofersoto@outlook.com>
2026-04-17 22:33:28 -0400

    adding JSDoc for restoreState

--- scripts/window.js
+++ scripts/window.js
@@ -1,6 +1,20 @@
 /********************************************************
  * @function restoreState
- * @descr Restores application state based on the state object 
+ * @descr
+ * Restores application state based on the state object 
+ * passed in.
+ * @param state: The state object to restore
+ * @global GlobalHistoryLogging: A boolean flag to control
+ * whether to log history state changes
+ * @global GlobalDialogClose: The dialog close button to
+ * click to close an open dialog box, if one exists
+ * @global GlobalModeActionButtons: Array of mode action
+ * buttons
+ * @global GlobalDialogCancelButtons: Array of dialog box
+ * cancel buttons
+ * @global GlobalModeTabButtons: Array of mode tab buttons
+ * @global GlobalModeTabPanels: Array of mode tab panels
+ * @global GlobalModeNamesMap: Maps mode names to integer
  ********************************************************/
 function restoreState(state) {
   console.log('Console: In restoreState: state:', JSON.stringify(state), 
