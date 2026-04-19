f8efac92c62a1f180f6f6ad44f0b38d24844e291
Jackson Pope <jacksonpope2@gmail.com>
2026-04-19 11:21:14 -0700

    Implemented popstate event listener

--- window.js
+++ window.js
@@ -49,5 +49,10 @@ window.addEventListener('load', (event) => {
 
 
 window.addEventListener('popstate', (event) => {
-
+    console.log('Console: In popstate: state:', JSON.stringify(event.state));
+  if (!event.state) {
+    console.log('Console: In popstate: No state associated with this history entry.');
+    return;
+  }
+  restoreState(event.state);
 });
