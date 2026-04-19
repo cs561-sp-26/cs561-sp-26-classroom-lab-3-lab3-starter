fd340fe125b1b4a7052249623e9fca4997082d59
kaveenaya <kaveeom@gmail.com>
2026-04-19 16:49:37 -0700

    Implement menu button click handler to toggle side menu

--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -1,2 +1,24 @@
 const GlobalMenuBtn = document.getElementById("menuBtn");
 const GlobalMenuItems = document.querySelectorAll("#sideMenu li");
+
+document.getElementById("menuBtn")
+  .addEventListener("click", function () {
+    const sideMenu = document.getElementById("sideMenu");
+    const sideMenuIcon = document.getElementById("menuBtnIcon");
+    const sideMenuBtn = document.getElementById("menuBtn");
+
+    if (sideMenuIcon.classList.contains("fa-bars")) {
+        sideMenuIcon.classList.remove("fa-bars");
+        sideMenuIcon.classList.add("fa-times");
+
+        sideMenuBtn.setAttribute("aria-expanded","true"); 
+        sideMenu.classList.add("sidemenu-open");
+
+    } else {
+        sideMenuIcon.classList.remove("fa-times");
+        sideMenuIcon.classList.add("fa-bars");
+
+        sideMenuBtn.setAttribute("aria-expanded","false");
+        sideMenu.classList.remove("sidemenu-open");
+    }
+});
