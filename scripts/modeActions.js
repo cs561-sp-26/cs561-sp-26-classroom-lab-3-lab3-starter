fe87344eaf1dd7cd916e024d15fa00afddd6d89a
Nikomixo <kevint0218@outlook.com>
2026-04-19 10:58:56 -0700

    Implemented switchMode

--- scripts/modeActions.js
+++ scripts/modeActions.js
@@ -18,6 +18,28 @@
  * @global dialogActionButtons: array of default ("OK") buttons for
  * each mode's dialog box
  *************************************************************************/
+ function switchMode(newMode) {
+    //Switch mode button
+    GlobalModeTabButtons[GlobalCurrentMode.get()].classList.remove("modetab-selected");
+    GlobalModeTabButtons[GlobalCurrentMode.get()].setAttribute("aria-selected",false);
+    GlobalModeTabButtons[newMode].classList.add("modetab-selected");
+    GlobalModeTabButtons[newMode].setAttribute("aria-selected",true);
+    //Switch tab panel
+    GlobalModeTabPanels[GlobalCurrentMode.get()].classList.add("hidden");
+    GlobalModeTabPanels[newMode].classList.remove("hidden");
+    GlobalCurrentMode.set(newMode); //Change mode
+    GlobalFocusedMode.set(newMode); //Change focused mode
+    if (GlobalHistoryLogging) {
+        const historyObj = {
+            page: GlobalModeNumbersToModes.get(newMode),
+            mode: newMode,
+            path: GlobalModeNumbersToRoutes.get(newMode)  
+        };   
+        history.pushState(historyObj, "", historyObj.path);
+        console.log("Console: In switchMode; pushing state: ", 
+            JSON.stringify(historyObj));
+    }
+}
 
 /*************************************************************************
  * @function Dialog Box Action Button CLICK handler 
