5706a611f8cad1405817741211fc67952b603a29
Alex Bruzda <alexbruzda@gmail.com>
2026-04-18 21:43:42 -0700

    branch:feature/menu-logic - add global variables for side menu tracking

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,19 @@
  *************************************************************************/
 let GlobalHistoryLogging = true;
 let GlobalDialogClose = null;
+/************************************/
+/* MENU VARIABLES                   */
+/************************************/
+const GlobalMenuBtn = document.getElementById("menuBtn");
+const GlobalMenuItems = document.querySelectorAll("li[role='menuitem']");
+
+//Note: Per Josh Wulf's blog post, we implement all immutable global variables using
+//immediately invoked function expressions
+const GlobalFocusedMenuItem = (() => {
+    let _focusedMenuItem = 0
+    const Store = {
+        get: () => _focusedMenuItem,
+        set: val => (_focusedMenuItem = val)
+    }
+    return Object.freeze(Store)
+})()
