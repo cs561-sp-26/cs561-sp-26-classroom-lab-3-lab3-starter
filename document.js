9ed7bfbca78447a7edaa66c302e4bc68fc56f82f
Jackson Pope <jacksonpope2@gmail.com>
2026-04-19 11:35:23 -0700

    Added Document.js skeleton funtion and description

---
+++ document.js
@@ -0,0 +1,15 @@
+/*************************************************************************
+ * File: document.js
+ * These functions support interaction with the top-level document
+ * element.
+ ************************************************************************/
+
+/*************************************************************************
+ * @function Document Keydown Event Handler 
+ * @desc 
+ * When the user presses a key in the app, we interpret the
+ * keypress based on which user interface element currently has focus. 
+ *************************************************************************/
+ document.addEventListener("keydown", function(e) { 
+    
+ });
