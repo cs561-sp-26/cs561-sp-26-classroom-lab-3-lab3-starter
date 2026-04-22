0ca75457fdfb3ab14f2885eb69a6753b3e0cc262
William Huang <148852245+William-HuangWY@users.noreply.github.com>
2026-04-21 20:06:11 -0700

    Add focus handler for side menu

--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -44,3 +44,63 @@ for (let i = 0; i < GlobalMenuItems.length; ++i) {
       GlobalMenuBtn.click();
     });
 }
+
+
+/*************************************************************************
+* @function keyDownMenuBtnFocused
+* @desc 
+* Handle keypress when the menuBtn has the focus. Process 
+* the arrow keys, space, and enter. All other keys are ignored.
+* @param key
+* The code of the key that was pressed.
+*************************************************************************/
+function keyDownMenuBtnFocused(key) {
+    if (key === "ArrowDown" || key === "ArrowUp" ||
+            key === "Space" || key === "Enter") {
+            menuBtn.click(); //open menu
+            if (key === "ArrowUp") { //Focus on last item
+                GlobalFocusedMenuItem.set(GlobalMenuItems.length-1);
+            } else { //Focus on first item
+                GlobalFocusedMenuItem.set(0);
+            }
+            GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+        }
+}
+
+/*************************************************************************
+* @function keyDownMenuItemFocused
+* @desc 
+* Handle keypress when menu is open and an item has focus. Per Table
+* 4.1 from the book, we handle the following key presses: tab, enter
+* escape, up arrow, down arrow, home, and end. 
+* are the arrow keys, space, and enter. All other keys are ignored.
+* @param key
+* The code of the key that was pressed.
+* @globals
+ * GlobalFocusedMenuItem is the index of the currently focused menu item
+ * GlobalMenuItems is an array of the HTML elements that are menu items   
+ * menuBtn is a reference to the menu button HTML element
+*************************************************************************/
+function keyDownMenuItemFocused(key) {  
+    if (key == "Enter") { //Activate focused menu item
+        document.activeElement.click();
+    } else if (key === "Tab") { //Close menu
+       GlobalMenuBtn.click();
+    } else if (key == "Escape") { //Close menu
+        GlobalMenuBtn.click();
+        GlobalMenuBtn.focus();
+    } else if (key === "ArrowUp") {  //Focus on next item
+        GlobalFocusedMenuItem.set((GlobalFocusedMenuItem.get() - 1 + GlobalMenuItems.length)
+          % GlobalMenuItems.length);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } else if (key === "ArrowDown") {  //Focus on prev item                                                                                                                                             
+        GlobalFocusedMenuItem.set((GlobalFocusedMenuItem.get() + 1) % GlobalMenuItems.length);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } else if (key === "Home") { //Focus on first item
+        GlobalFocusedMenuItem.set(0);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } else if (key === "End") { //Focus on last item
+        GlobalFocusedMenuItem.set(GlobalMenuItems.length - 1);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } 
+}
